6db4f82386e49ddb7501e183f7657f69385140d3
dthabata <dthabatita@gmail.com>
2024-02-09 14:11:39 -0300

    fix: routes and calls routes in App

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import { Fragment } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from '../pages/Home';
-import { Signin } from '../pages/Signin';
-import { Signup } from '../pages/Singup';
+import Home from '../pages/Home'
+import Signin from '../pages/Signin';
+import Signup from '../pages/Singup';
 
 const Private = ({ Item }) => {
     const signed = false;
