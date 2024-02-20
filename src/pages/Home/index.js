5c59ab7e69aa35aac6ba94c59edcf3255501bbba
dthabata <dthabatita@gmail.com>
2024-02-20 11:37:48 -0300

    feat: add list to show Animal DB

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,21 +4,50 @@ import Button from '../../components/Button';
 import * as C from './styles';
 
 const Home = () => {
+
     const signout = () => {
         localStorage.removeItem("token");
         localStorage.removeItem("name");
     };
 
+    const user = localStorage.name;
+
     const navigate = useNavigate();
 
     return (
         <C.Container>
-            <C.Content>
-                <C.Title>Home</C.Title>
-                <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
-                    Sair
-                </Button>
-            </C.Content>
+            <C.Header>
+                <C.Title>Olá, {user}!</C.Title>
+                <C.ButtonsRow>
+                    <C.Button>
+                        <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
+                            Sair
+                        </Button>
+                    </C.Button>
+                    <C.Button>
+                        <Button Text="Cadastrar novo animal" onClick={() => [signout(), navigate("/")]}>
+                            Cadastrar novo animal
+                        </Button>
+                    </C.Button>
+                </C.ButtonsRow>
+            </C.Header>
+            <C.Subtitle>Esses são os animais cadastrados em nosso banco de dados:</C.Subtitle>
+            <C.Table>
+                <ul id="lista">
+                    <li>
+                        <p>Animal 1</p>
+                    </li>
+                    <li>
+                        <p>Animal 2</p>
+                    </li>
+                    <li>
+                        <p>Animal 3</p>
+                    </li>
+                    <li>
+                        <p>Animal 4</p>
+                    </li>
+                </ul>
+            </C.Table>
         </C.Container>
     );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,15 +1,36 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: row;
-    width: 100%;
+    margin: 0;
+    padding: 10px;
     height: 100vh;
-    gap: 20px;
 `;
 
-export const Content = styled.div``;
+export const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+    width: 100%;
+`;
 
-export const Title = styled.h2``;
+export const ButtonsRow = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: row-reverse;
+    gap: 10px;
+`;
+
+export const Button = styled.div``;
+
+export const Title = styled.h3``;
+
+export const Subtitle = styled.p``;
+
+export const Table = styled.table`
+    display: flex;
+    align-items: center;
+    justify-content: initial;
+    width: 100%;
+    padding: 20px;
+`;
